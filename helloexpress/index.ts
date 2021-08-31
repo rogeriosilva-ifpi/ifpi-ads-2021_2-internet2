67c527fadbf047973cd386a5a1d4a926cdc7dc92
Rogério Silva <rogerio.silva@ifpi.edu.br>
2021-08-31 16:09:11 -0300

    Backend Add CORS + FrontEnd List and Create Ok

--- helloexpress/index.ts
+++ helloexpress/index.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin'
 import express, {Request, Response} from 'express'
+import cors from 'cors'
 
 const app = express()
 
@@ -13,6 +14,7 @@ admin.initializeApp({
 const db = admin.firestore()
 
 app.use(express.json())
+app.use(cors())
 
 // rotas (Filme: id, nome, tema, ano, duracao)
 app.get('/filmes', async (req: Request, res: Response) => {
@@ -29,9 +31,9 @@ app.get('/filmes', async (req: Request, res: Response) => {
 })
 
 app.post('/filmes', async (req: Request, res: Response) => {
-    const {nome, tema, duracao, ano} = req.body
+    const {nome, genero, duracao, ano} = req.body
 
-    const filme = {nome, tema, duracao: Number(duracao), ano: Number(ano)}
+    const filme = {nome, genero, duracao: Number(duracao), ano: Number(ano)}
 
     const resultado = await db.collection('filmes').add(filme)
 
